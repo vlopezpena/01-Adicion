3052e98266ad6b53719f59466e0d544460a57e45
vlopezpena <37960063+vlopezpena@users.noreply.github.com>
2018-04-19 11:40:07 -0300

    Add files via upload

---
+++ README 2.md
@@ -0,0 +1,17 @@
+# AED
+Lopez Pena Vanessa
+K1051
+01-Adicion
+Problema
+Obtener del usuario dos números y mostrarle la suma
+
+Lexico
+a,b E Z
+
+Modelo IPO
+
+
+Representacion Textual: Pseudocódigo 
+Pedir y Leer a y Guardar en a.
+Pedir y Leer b y Guardar en b.
+Mostrar a+b.
---
+++ adicion.cpp
@@ -0,0 +1,22 @@
+/*
+ Lopez Pena Vanessa Guadalupe
+ AED
+ 19/04/2018
+*/
+
+#include <iostream>
+using namespace std;
+
+int main (){
+	int a=0, b=0;
+
+	cout << "Numero 1: ";
+	cin >> a;
+
+	cout << "\nNumero 2: ";
+	cin >> b;
+
+	cout << "\nLa suma es: "<< a+b <<endl;
+
+	return 0;
+}
